a9a600bbd3380f43ef47bf25b9132fb2c61e8d5a
Jagoba Gascón <jagoba@arima.eu>
2024-11-28 13:15:08 +0100

    use a script

--- build.sh
+++ build.sh
@@ -4,28 +4,21 @@ set -e
 set -u
 
 # install homebrew
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # add homebrew to path
 #echo >> /home/arch/.bashrc
 #echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/arch/.bashrc
-#eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install Homebrew's dependencies if you have sudo access:
-#sudo pacman --noconfirm -S base-devel
+sudo pacman --noconfirm -S base-devel
 
 # install fastlane
+brew install xcodesorg/made/xcodes
 #brew install fastlane
-#brew install xcodesorg/made/xcodes
 
 # download xcodes
-curl -L https://github.com/XcodesOrg/xcodes/releases/download/1.6.0/xcodes-1.6.0.mojave.bottle.tar.gz -o xcodes.tar.gz
-tar -xvf xcodes.tar.gz
-sudo mv xcodes/1.6.0/bin/xcodes /usr/local/bin
-chmod +x /usr/local/bin/xcodes
-rm -r xcodes
-rm xcodes.tar.gz
-
 xcodes version 15.3
 
 xcodebuild -version
